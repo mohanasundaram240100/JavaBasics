89a24681915c1712861e14ab71edd58940a31f38
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-03-18 22:06:27 +0530

    Cleaned up

--- src/basicprograms/Stringoccurence.java
+++ src/basicprograms/Stringoccurence.java
@@ -1,25 +1,27 @@
 package basicprograms;
 
 public class Stringoccurence {
-	
-	
+
+
 	public static void main (String[] args) {
-		
+
 		String input = "Swiss"; //input value
 		char toFind = 'S'; //need to find the character
 		int present = 0;   //initial value 0
-		
+
 		input.toLowerCase();  //converting the case sensitive values to lowercase values
-		
+
 		for(int i=0;i<input.length();i++) {
-			
-			if(input.charAt(i)==toFind);  
+
+			if(input.charAt(i)==toFind) {
+
+			}
 			present=present++;
-						
+
 		}
-		
+
 		System.out.println(present);
-		
+
 	}
 
 }
