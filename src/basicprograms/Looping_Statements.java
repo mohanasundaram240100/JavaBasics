9c19935d7baa7a81caaf8732ca2053816a7a26eb
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-03-24 21:36:30 +0530

    Loop commit

---
+++ src/basicprograms/Looping_Statements.java
@@ -0,0 +1,52 @@
+package basicprograms;
+
+public class Looping_Statements {
+
+	public static void main(String[] args) {
+
+		//While loop example  (Printing integers and Strings)
+		
+		int i=1;    	//Initialization
+		
+		while(i<=15)    //Condition
+		{
+			System.out.println(i);
+			i++;        //Increment/Decrement 
+			
+		}
+		
+		
+		//Example 2
+		
+		int j =1;		
+		
+		while(j<=10)
+		{
+			System.out.println("hello");
+			j++;
+			
+		}
+		
+		
+		//Example 2   Printing even numbers from 1 to 10
+		
+		int k=1;
+		
+		while(k<=10)
+		{
+			if(k%2==0)
+			{
+			System.out.println(k);
+			}
+			
+			k++;
+			
+			
+		}
+		
+		
+		
+		
+	}
+
+}
