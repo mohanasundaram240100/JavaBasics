90d0982186f2e447c313e3a07bb3189b4e059ba8
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-04-02 22:17:27 +0530

    DC

---
+++ src/basicprograms/ArrayList.java
@@ -0,0 +1,21 @@
+package basicprograms;
+
+import java.util.List;
+
+public class ArrayList {
+	
+	public static void main(String[] args) {
+		
+		ArrayList arr = new ArrayList();
+		
+		List<String> arr1 = new ArrayList<String>();
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
