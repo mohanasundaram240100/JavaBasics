303816169dcf23b5255d834143beb1909d8705cb
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-05-03 17:55:46 +0530

    DC

---
+++ src/Collections/ArrayList.java
@@ -0,0 +1,20 @@
+package Collections;
+
+import java.util.List;
+
+public class ArrayList {
+	
+	public static void main (String[] args) {
+		
+		//Declaration of ArrayList
+		
+		ArrayList mylist = new ArrayList();
+		
+		
+		
+		
+		
+		
+	}
+
+}
