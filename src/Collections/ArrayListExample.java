9a6a5c56175a1910f5a705df5d206b5b95dfaa29
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-05-05 19:15:37 +0530

    DC

---
+++ src/Collections/ArrayListExample.java
@@ -0,0 +1,57 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayListExample {
+	
+	public static void main (String[] args) {
+		
+		//Declaration of ArrayList
+		
+		ArrayList mylist = new ArrayList();
+		
+		//Adding elements to the ArrayList
+	
+		mylist.add("Mohan");
+		mylist.add(100);
+		mylist.add(10.5);
+		mylist.add('A');
+		
+		System.out.println(mylist);
+		
+		ArrayList mylist1 = new ArrayList();
+		mylist1.add("Rohan");
+		mylist.set(1, "Mohan");
+		System.out.println(mylist);
+		
+		//Using for loop
+		
+		for(int i=0;i<mylist.size();i++) 
+		{
+		
+			System.out.println(mylist.get(i));
+		}
+		
+		
+		//Using for each loop
+		
+		for(Object list :mylist)
+		{
+			System.out.println(list);
+		}
+		
+		
+		//Using iterator	
+		
+		Iterator<Object> lists=mylist.iterator();
+		while(lists.hasNext())
+		{
+			System.out.println(lists.next());
+			
+		}
+		
+	}
+
+}
