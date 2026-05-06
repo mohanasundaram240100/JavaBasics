28434217d83b236f8e2c22a60356fac8a6975d0a
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-05-06 20:38:52 +0530

    DC

---
+++ src/Variables_DataTypes/Variable_DataTypes.java
@@ -0,0 +1,15 @@
+package Variables_DataTypes;
+
+public class Variable_DataTypes {
+
+	public static void main (String[] args) {
+		
+		
+		int a = 10;
+		float b= 12.51f;
+		double c = 120.2;
+		char d = 'A';
+		String e = "Mohan";
+	}
+
+}
