90a5cdef65b6bcc976c1cbdfa0ed0f6c10758b09
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-05-09 17:08:29 +0530

    DC

--- src/Variables_DataTypes_Operators/Operators.java
+++ src/Variables_DataTypes_Operators/Operators.java
@@ -7,9 +7,38 @@ public class Operators {
 		int a = 182;
 		int b = 9071;
 		
-		int result = a+b;
+		//Arithmetic Operators
 		
+		int result = a+b;
 		System.out.println(result);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		//Relational Operators
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		
+		
+		//Logical Operators
+		System.out.println((a>b) && (a<b));
+		System.out.println((a>b) || (a<b));
+		System.out.println(!(a>b));
+		
+		
+		//Increment and Decrement Operators
+		
+		int c = 10;
+		System.out.println(c++); //Post Increment
+		System.out.println(++c); //Pre Increment
+		System.out.println(c--); //Post Decrement
+		System.out.println(--c); //Pre Decrement
+		
+		
+		
 		
 	}
 	
