1532aee697ee81099516ad67aab85dfdd6629205
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-05-11 21:07:32 +0530

    DC

--- src/Arrays/SingleDimArray.java
+++ src/Arrays/SingleDimArray.java
@@ -8,7 +8,23 @@ public class SingleDimArray {
 		
 		int[] arr = {1, 2, 3, 4, 5};
 
+		//Normal for loop
 		
+		for(int i=0;i<=arr.length-1;i++) {
+			
+			System.out.println(arr[i]);
+		}
+		
+		
+		
+		int[] b = {11, 22 , 33, 44, 55};
+		
+		//Enhanced for loop
+		
+		for (int x : b)
+		{
+			System.out.println(x);
+		}
 
 		
 		
