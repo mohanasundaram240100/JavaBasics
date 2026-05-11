fb693ef66b3f31afb74652b101c1bc2d312d8681
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-05-11 20:48:13 +0530

    DC

--- src/Arrays/SingleDimArray.java
+++ src/Arrays/SingleDimArray.java
@@ -7,13 +7,12 @@ public class SingleDimArray {
 		
 		
 		int[] arr = {1, 2, 3, 4, 5};
-		
-		System.out.println(arr[0]);
-		System.out.println(arr[1]);
-		System.out.println(arr[2]);
-		System.out.println(arr[3]);
-		System.out.println(arr[4]);
 
+		
+
+		
+		
+		
 	}
 
 }
