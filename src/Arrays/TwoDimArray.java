fbcbdbc5b131ddc41437ff310464a3d05ae3f269
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-05-11 21:40:27 +0530

    DC

--- src/Arrays/TwoDimArray.java
+++ src/Arrays/TwoDimArray.java
@@ -5,10 +5,38 @@ public class TwoDimArray {
 	public static void main (String[] args) {
 		
 		
-		int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+		int[][] arr =  {{1,2,3},
+						{4,5,6},
+						{7,8,9}};
+		
+		//Normal for loop
+		
+		for (int i=0;i<=arr.length-1;i++)
+		{
+			
+			for(int j=0;j<arr[i].length;j++) 
+			{
+				
+				
+				System.out.print(arr[i][j] + " ");
+			}
+			
+			System.out.println();
+			
+		}
 		
 		
+		//Enhanced for loop
 		
+		for (int[] x : arr)
+		{
+			for (int y : x)
+			{
+				System.out.print(y + " ");
+			}
+			
+			System.out.println();
+		}
 		
 		
 		
