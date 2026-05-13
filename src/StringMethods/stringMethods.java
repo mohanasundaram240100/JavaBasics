ba7164d1b57e9cb3d4dab295aaeb57e2548442b3
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-05-13 12:13:12 +0530

    DC

---
+++ src/StringMethods/stringMethods.java
@@ -0,0 +1,31 @@
+package StringMethods;
+
+public class stringMethods {
+	
+	public static void main (String[] args) {
+		
+		String name ="Mohanasundaram";
+		System.out.println(name.contains("Mohana"));
+		
+		
+		System.out.println(name.charAt(0));
+		System.out.println(name.charAt(1));
+		System.out.println(name.charAt(2));
+		System.out.println(name.charAt(3));
+		System.out.println(name.charAt(4));
+		System.out.println(name.charAt(5));
+
+		
+		System.out.println(name.equals("Mohanasundaram"));
+		
+		System.out.println(name.equals("mohanasundaram"));
+		
+		System.out.println(name.equalsIgnoreCase("mohanaSundaram"));
+	
+	
+	
+	}
+	
+	
+
+}
