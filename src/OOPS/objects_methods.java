2db87a38d3955703cfa3d2bdd608ed588ff066b1
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-05-14 21:38:01 +0530

    DC

---
+++ src/OOPS/objects_methods.java
@@ -0,0 +1,30 @@
+package OOPS;
+
+public class objects_methods {
+	
+	//Class contains variables and methods
+	//Object is a instance of class
+	
+	
+	//Variables
+	
+	String name;
+	int age;
+	String desig;
+	
+	//Methods
+	
+	public static void employee()
+	{
+		System.out.println("Name");
+	}
+	
+	
+	public static void main (String[] args) {
+		
+		
+		
+		
+	}
+
+}
