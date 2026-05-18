dff576767252b693d3596a28d30ad59c8de344e1
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-05-18 21:09:03 +0530

    DC

---
+++ src/Polymorphism/PolymorphismMainMethod.java
@@ -0,0 +1,22 @@
+package Polymorphism;
+
+public class PolymorphismMainMethod {
+	
+	public static void main (String[] args)
+	{
+		
+		PolymorphismMethods pm = new PolymorphismMethods();
+		pm.adder();
+		pm.adder(12,25,65);
+		pm.adder(150, 205);
+		pm.adder(10, 25.5);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/Polymorphism/PolymorphismMethods.java
@@ -0,0 +1,30 @@
+package Polymorphism;
+
+public class PolymorphismMethods {
+	
+	
+	public int x=10;
+	public int y=20;
+	
+	public void adder()
+	{
+		System.out.println(x+y);
+	}
+	
+	public void adder(int a, int b)
+	{
+		System.out.println(a+b);
+	}
+	
+	public void adder(int x, double y)
+	{
+		System.out.println(x+y);
+	}
+	
+	public void adder(int x, int y, int z)
+	{	
+		System.out.println(x+y+z);
+		
+	}
+
+}
