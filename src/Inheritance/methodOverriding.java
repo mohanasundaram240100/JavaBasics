ef55818d1120c33550ec90c6c8238f9a2abaef00
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-05-24 12:06:56 +0530

    DC

---
+++ src/Inheritance/methodOverriding.java
@@ -0,0 +1,41 @@
+package Inheritance;
+
+
+class parent
+{
+	int A=100;
+	int B=200;
+	
+	public void method1()
+	{
+		System.out.println(A);
+		System.out.println(B);
+	}
+}
+
+class child extends parent
+{
+	int A=300;
+	int B=400;
+	
+	public void method1()
+	{
+		System.out.println(A);
+		System.out.println(B);
+	}
+}
+
+public class methodOverriding 
+{
+	public static void main (String[] args)
+	{
+		child ch = new child();
+		ch.method1();
+		
+		
+		
+	}
+	
+	
+
+}
