250bc0bd8ffee04a402d4aca1f3666a685c114fa
mohanasundaramp <mohanasundaramp@localhost>
2026-05-30 19:21:58 +0530

    DC

---
+++ src/JavaProblems/reverseString.java
@@ -0,0 +1,20 @@
+package JavaProblems;
+
+public class reverseString {
+	
+	public static void main (String[] args) {
+		
+		String input ="Mohanasundaram";
+		String rev ="";
+		
+		for (int i=input.length()-1;i>=0;i--)
+		{
+			rev=rev+input.charAt(i);
+		}
+		
+		System.out.println("Reverse of the string is: " + rev);
+		
+		
+	}
+
+}
